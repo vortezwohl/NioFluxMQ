eb865a8be6f0de123c524f8c3bba9b553ce86504
吴子豪 <vortezwohl@proton.me>
2025-08-30 03:11:25 +0800

    feat: 开发JSONdump处理器

---
+++ nioflux_mq/handler/json_dump_handler.py
@@ -0,0 +1,20 @@
+import asyncio
+import json
+
+from typing_extensions import Any
+from typing_extensions import override
+
+from nioflux.pipeline.stage import PipelineStage
+
+from nioflux_mq.mq import MessageQueue
+
+
+class JsonDumpHandler(PipelineStage):
+    def __init__(self):
+        super().__init__(label='json_load_handler')
+
+    @override
+    async def __call__(self, data: dict, extra: MessageQueue, err: list[Exception], fire: bool,
+                       io_ctx: tuple[asyncio.StreamReader, asyncio.StreamWriter] | None) -> tuple[Any, Any, list[Exception], bool]:
+        data = json.dumps(data, ensure_ascii=False)
+        return data, extra, err, fire

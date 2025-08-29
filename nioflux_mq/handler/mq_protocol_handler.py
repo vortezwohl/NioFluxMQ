d2366ca5c09c9b2b65eef4397525e15c82b20c99
吴子豪 <vortezwohl@proton.me>
2025-08-30 02:43:13 +0800

    feat: 开发基础的协议解析器

---
+++ nioflux_mq/handler/mq_protocol_handler.py
@@ -0,0 +1,53 @@
+import asyncio
+import os
+
+from typing_extensions import Any, override
+
+from nioflux import PipelineStage
+
+from nioflux_mq.snapshot import __PATH__
+from nioflux_mq.mq.message_queue import MessageQueue
+
+
+class NioFluxMQProtocolHandler(PipelineStage):
+    def __init__(self):
+        super().__init__(label='nioflux_mq_protocol_handler')
+
+    @override
+    async def __call__(self, data: dict, extra: MessageQueue, err: list[Exception], fire: bool,
+                       io_ctx: tuple[asyncio.StreamReader, asyncio.StreamWriter] | None) -> tuple[Any, Any, list[Exception], bool]:
+        mq = extra
+        instruction = data['instruction']
+        payload = data['payload']
+        snapshot = data['snapshot']
+        resp = {'success': True, 'info': None, 'err': []}
+        try:
+            match instruction:
+                case 'register_topic':
+                    mq.register_topic(**payload)
+                case 'unregister_topic':
+                    mq.unregister_topic(**payload)
+                case 'register_consumer':
+                    mq.register_consumer(**payload)
+                case 'unregister_consumer':
+                    mq.unregister_consumer(**payload)
+                case 'produce':
+                    payload['message'] = payload['message'].encode('utf-8')
+                    mq.produce(**payload)
+                case 'peek':
+                    resp['info'] = mq.peek(**payload)
+                case 'consume':
+                    resp['info'] = mq.consume(**payload)
+                case 'advance':
+                    mq.advance(**payload)
+                case 'retreat':
+                    mq.retreat(**payload)
+                case _:
+                    raise ValueError(f'Unsupported instruction: {instruction}')
+        except Exception as e:
+            err.append(e)
+            resp['success'] = False
+        finally:
+            if snapshot:
+                extra.save(path=os.getenv('MQ_SNAPSHOT_PATH', __PATH__))
+        return resp, mq, err, fire

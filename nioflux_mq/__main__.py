0ea8aa60fe823c50dc0f816aa1063363889f75d8
吴子豪 <vortezwohl@proton.me>
2025-08-30 03:11:53 +0800

    feat: MQ服务器与测试客户端实现

--- nioflux_mq/__main__.py
+++ nioflux_mq/__main__.py
@@ -4,13 +4,14 @@ from nioflux import Server, StrDecode, StrEncode, ErrorNotify
 
 from nioflux_mq.mq import MessageQueue
 from nioflux_mq.handler.json_load_handler import JsonLoadHandler
+from nioflux_mq.handler.json_dump_handler import JsonDumpHandler
 from nioflux_mq.handler.mq_protocol_handler import NioFluxMQProtocolHandler
 from nioflux_mq.handler.response_handler import ResponseHandler
 
 MQ = MessageQueue()
-server = Server(pipeline=[StrEncode(), JsonLoadHandler(), NioFluxMQProtocolHandler(),
-                          StrDecode(), ErrorNotify(), ResponseHandler()],
-                extra=MQ)
+server = Server(pipeline=[StrDecode(), JsonLoadHandler(), NioFluxMQProtocolHandler(),
+                          JsonDumpHandler(), StrEncode(), ErrorNotify(), ResponseHandler()],
+                port=23861, extra=MQ)
 
 
 async def main():
